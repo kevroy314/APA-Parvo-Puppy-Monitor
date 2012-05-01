aa28fc2beec56266fdf27bfc4267ec1ddb5391ad
unknown <khorecka@AE-KHORECKA.amer.corp.natinst.com>
2012-05-01 12:30:00 -0500

    -removed reset script as it exists in style.css -moved general style into parvo_monitor_style.css -replaced header, footer and sidebar with javascript functions for modularity -replaced fade transitions and page scroll animations with script files -added page template

---
+++ js/parvo-monitor-header.js
@@ -0,0 +1,1 @@
+$(function(){$("#parvo_monitor_header").html('<h1 id="logo"><a href="http://www.austinpetsalive.org/" title="Austin Pets Alive!"><img src="./images/logo_apa.png" alt="Austin Pets Alive!"></a></h1><div id="parvo_monitor_title_div">Parvo Puppy Status Monitor</div>');});
